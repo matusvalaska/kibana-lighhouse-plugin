81cdc338561be81cbd2c242c3b68047a1d41daca
matusvalaska <m.valaska@gmail.com>
2020-10-19 09:22:05 +0200

    update linting

--- public/components/main/main.js
+++ public/components/main/main.js
@@ -7,17 +7,38 @@ import {
   EuiPageContent,
   EuiPageContentHeader,
   EuiPageContentBody,
+  EuiDatePicker,
+  EuiDatePickerRange,
   EuiText,
 } from '@elastic/eui';
 // import { FormattedMessage } from '@kbn/i18n/react';
 import { SummaryHeading } from '../summaryHeading';
+import moment from 'moment';
 import ReportViewer from 'react-lighthouse-viewer';
 import jsonReport from '/Users/matusvalaska/Documents/GitHub_Repo/kibana/plugins/kibana-lighhouse-plugin/data/www.csob.sk-20201019T005738.json';
 
 export class Main extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      startDate: moment(),
+      endDate: moment().add(11, 'd'),
+    };
+
+    this.handleChangeStart = this.handleChangeStart.bind(this);
+    this.handleChangeEnd = this.handleChangeEnd.bind(this);
+  }
+
+  handleChangeStart(date) {
+    this.setState({
+      startDate: date,
+    });
+  }
+
+  handleChangeEnd(date) {
+    this.setState({
+      endDate: date,
+    });
   }
 
   componentDidMount() {
@@ -40,21 +61,45 @@ export class Main extends React.Component {
             </EuiTitle>
           </EuiPageHeader>
           <EuiPageContent>
-            {/*<EuiPageContentHeader>*/}
-              {/*<EuiTitle>*/}
-              {/*  <h2>Congratulations</h2>*/}
-              {/*</EuiTitle>*/}
-              {/*<SummaryHeading />*/}
-            {/*  <div>*/}
-            {/*    <hr className="euiHorizontalRule euiHorizontalRule--full euiHorizontalRule--marginLarge" />*/}
-            {/*  </div>*/}
-            {/*</EuiPageContentHeader>*/}
+            <EuiPageContentHeader>
+              <EuiPageContent>
+                <EuiDatePickerRange
+                  startDateControl={
+                    <EuiDatePicker
+                      selected={this.state.startDate}
+                      onChange={this.handleChangeStart}
+                      startDate={this.state.startDate}
+                      endDate={this.state.endDate}
+                      isInvalid={this.state.startDate > this.state.endDate}
+                      aria-label="Start date"
+                      showTimeSelect
+                    />
+                  }
+                  endDateControl={
+                    <EuiDatePicker
+                      selected={this.state.endDate}
+                      onChange={this.handleChangeEnd}
+                      startDate={this.state.startDate}
+                      endDate={this.state.endDate}
+                      isInvalid={this.state.startDate > this.state.endDate}
+                      aria-label="End date"
+                      showTimeSelect
+                    />
+                  }
+                />
+                {/*<EuiTitle>*/}
+                {/*  <h2>Congratulations</h2>*/}
+                {/*</EuiTitle>*/}
+                {/*<SummaryHeading />*/}
+                {/*  <div>*/}
+                {/*    <hr className="euiHorizontalRule euiHorizontalRule--full euiHorizontalRule--marginLarge" />*/}
+                {/*  </div>*/}
+              </EuiPageContent>
+            </EuiPageContentHeader>
             <EuiPageContentBody>
-              {/*<EuiText>*/}
-              {/*  <h3>You have successfully created your first Kibana Plugin!</h3>*/}
-              {/*  <p>The server time (via API call)</p>*/}
-              {/*</EuiText>*/}
-              <ReportViewer json={jsonReport} />
+              <EuiPageContent>
+                <ReportViewer json={jsonReport} />
+              </EuiPageContent>
             </EuiPageContentBody>
           </EuiPageContent>
         </EuiPageBody>
