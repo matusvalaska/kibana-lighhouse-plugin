1d1b520e4b0054c865f50c05933e7bc42bc82460
matusvalaska <m.valaska@gmail.com>
2020-10-23 11:12:40 +0200

    change esHost protocol

--- server/routes/elasticApi.js
+++ server/routes/elasticApi.js
@@ -2,7 +2,7 @@ import es from 'elasticsearch';
 // const { Client } = require('@elastic/elasticsearch');
 
 // const elasticsearchHost = 'http://192.168.1.11:9200';
-const elasticsearchHost = 'http://10.244.7.29:9200';
+const elasticsearchHost = 'https://10.244.7.29:9200';
 const elasticsearchIndex = 'speed-000003';
 
 function getIndexRequestBody(gte, lte) {
